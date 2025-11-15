60ca0b260fb218960cf8c7108c8d073e49158954
Casey Rodarmor <casey@rodarmor.com>
2025-11-14 20:33:37 -0800

    Reform

--- src/image.rs
+++ src/image.rs
@@ -1,4 +1,7 @@
-use super::*;
+use {
+  super::*,
+  png::{ColorType, Compression, Encoder},
+};
 
 #[derive(Default, Debug, PartialEq)]
 pub(crate) struct Image {
@@ -23,18 +26,124 @@ impl Image {
 
     let writer = BufWriter::new(file);
 
-    let mut encoder = png::Encoder::new(writer, self.width, self.height);
-    encoder.set_color(png::ColorType::Rgba);
-    encoder.set_depth(png::BitDepth::Eight);
+    let mut alpha = false;
+    let mut color = false;
+    for chunk in self.data.chunks(4) {
+      let chunk: [u8; 4] = chunk.try_into().unwrap();
+      let [r, g, b, a] = chunk;
+      if a != u8::MAX {
+        alpha = true;
+      }
+      if r != g || r != b {
+        color = true;
+      }
+    }
+
+    let color_type = match (color, alpha) {
+      (false, false) => ColorType::Grayscale,
+      (false, true) => ColorType::GrayscaleAlpha,
+      (true, false) => ColorType::Rgb,
+      (true, true) => ColorType::Rgba,
+    };
+
+    let data = match color_type {
+      ColorType::Grayscale => Cow::Owned(
+        self
+          .data
+          .chunks(4)
+          .map(|chunk| chunk[0])
+          .collect::<Vec<u8>>(),
+      ),
+      ColorType::GrayscaleAlpha => Cow::Owned(
+        self
+          .data
+          .chunks(4)
+          .flat_map(|chunk| [chunk[0], chunk[3]])
+          .collect::<Vec<u8>>(),
+      ),
+      ColorType::Rgb => Cow::Owned(
+        self
+          .data
+          .chunks(4)
+          .flat_map(|chunk| &chunk[0..3])
+          .copied()
+          .collect::<Vec<u8>>(),
+      ),
+      ColorType::Rgba => Cow::Borrowed(&self.data),
+      ColorType::Indexed => unreachable!(),
+    };
+
+    let mut encoder = Encoder::new(writer, self.width, self.height);
+    encoder.set_color(color_type);
+    encoder.set_compression(Compression::High);
 
     let mut writer = encoder.write_header().context(error::PngEncode { path })?;
 
     writer
-      .write_image_data(&self.data)
+      .write_image_data(&data)
       .context(error::PngEncode { path })?;
 
     writer.finish().context(error::PngEncode { path })?;
 
     Ok(())
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn color_type_reduction() {
+    #[track_caller]
+    fn case(dir: &Path, data: &[u8], color_type: ColorType, expected: &[u8]) {
+      let image = Image {
+        data: data.into(),
+        width: 2,
+        height: 1,
+      };
+
+      let path = dir.join("image.png");
+
+      image.save(&path).unwrap();
+
+      let decoder = png::Decoder::new(BufReader::new(File::open(path).unwrap()));
+      let mut reader = decoder.read_info().unwrap();
+      let mut buffer = vec![0; reader.output_buffer_size().unwrap()];
+      let info = reader.next_frame(&mut buffer).unwrap();
+      assert_eq!(info.color_type, color_type);
+      let bytes = &buffer[..info.buffer_size()];
+      assert_eq!(bytes, expected);
+    }
+
+    let tempdir = tempfile::tempdir().unwrap();
+
+    case(
+      tempdir.path(),
+      &[0, 0, 0, 255, 255, 255, 255, 255],
+      ColorType::Grayscale,
+      &[0, 255],
+    );
+
+    case(
+      tempdir.path(),
+      &[0, 0, 0, 255, 255, 255, 255, 127],
+      ColorType::GrayscaleAlpha,
+      &[0, 255, 255, 127],
+    );
+
+    case(
+      tempdir.path(),
+      &[0, 0, 0, 255, 0, 127, 255, 255],
+      ColorType::Rgb,
+      &[0, 0, 0, 0, 127, 255],
+    );
+
+    case(
+      tempdir.path(),
+      &[0, 0, 0, 255, 0, 127, 255, 127],
+      ColorType::Rgba,
+      &[0, 0, 0, 255, 0, 127, 255, 127],
+    );
   }
 }
